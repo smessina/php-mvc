8df44a3dbcf8a969aaa37cd8cb4a150ce80783e4
= <info@silviomessina.com>
2018-04-17 18:37:28 -0300

    Correcciones y ajustes

--- controlador/producto.php
+++ controlador/producto.php
@@ -7,13 +7,14 @@
 	$datos =  $unProducto->getProducto();
 
 	$plantilla = file_get_contents('vista/plantillas/catalogo.html');
+	
 	$ficha = file_get_contents('vista/producto.html');
 
 	$diccionario = array("{NOMBRE}", "{PRECIO}", "{STOCK}");
 
-	$ficha = str_replace($datos, $diccionario, $ficha);
+	$catalogo = str_replace($diccionario, $datos, $ficha);
 
-	echo str_replace($ficha, "{CATALOGO}", $plantilla);
+	echo str_replace("{CATALOGO}", $catalogo, $plantilla);
 
 
 ?>
